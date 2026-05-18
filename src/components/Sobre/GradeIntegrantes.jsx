222a9e17c1359fa3977138347ab472fc38b4b9f7
Danilo <danilojmini2@gmail.com>
2026-05-18 09:52:42 -0300

    Ajustando organização de grade de integrantes

--- src/components/Sobre/GradeIntegrantes.jsx
+++ src/components/Sobre/GradeIntegrantes.jsx
@@ -2,18 +2,49 @@ import CartaoIntegrante from './CartaoIntegrante.jsx';
 import './GradeIntegrantes.css';
 
 export default function GradeIntegrantes({ integrantes = [] }) {
-    return (
-        <section className="secaoIntegrantes" aria-label="Integrantes">
-            <div className="tituloSecaoIntegrantes">
-                <h2>Integrantes do projeto</h2>
-                <p>Veja quem fez nosso sonho acontecer</p>
-            </div>
+    const equipeConteudo = integrantes.filter(
+        (integrante) =>
+            integrante.curso === 'Mecânica.' || integrante.curso === 'Eletro-eletrônica.',
+    );
 
-            <div className="gradeIntegrantes">
-                {integrantes.map((integrante) => (
-                    <CartaoIntegrante key={integrante.id} integrante={integrante} />
-                ))}
-            </div>
-        </section>
+    const equipeTecnologia = integrantes.filter(
+        (integrante) =>
+            integrante.curso !== 'Mecânica.' && integrante.curso !== 'Eletro-eletrônica.',
+    );
+
+    return (
+        <>
+            <section className="secaoIntegrantes">
+                <div className="tituloSecaoIntegrantes">
+                    <div>
+                        <h2>Equipe de Tecnologia</h2>
+
+                        <p>Alunos de Desenvolvimento de Sistemas (SENAI)</p>
+                    </div>
+                </div>
+
+                <div className="gradeIntegrantes">
+                    {equipeTecnologia.map((integrante) => (
+                        <CartaoIntegrante key={integrante.id} integrante={integrante} />
+                    ))}
+                </div>
+            </section>
+
+            <section className="secaoIntegrantes">
+                <div className="tituloSecaoIntegrantes">
+                    <div>
+                        <h2>Equipe de Conteúdo</h2>
+
+                        <p>Alunos de Mecânica e Eletro-eletrônica (SESI)</p>
+                    </div>
+                </div>
+
+                <div className="gradeIntegrantes">
+                    {equipeConteudo.map((integrante) => (
+                        <CartaoIntegrante key={integrante.id} integrante={integrante} />
+                    ))}
+                </div>
+            </section>
+        </>
     );
 }
